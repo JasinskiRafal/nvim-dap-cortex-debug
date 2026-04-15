437a8f7e16fc334c0c9accd8c2644c9ccf38715b
Rafal Jasinski <rajs@softwaremind.com>
2026-04-15 17:26:47 +0200

    Removed the warning causing circular dependency

--- lua/dap-cortex-debug/config.lua
+++ lua/dap-cortex-debug/config.lua
@@ -1,5 +1,4 @@
 local M = {}
-local utils = require('dap-cortex-debug.utils')
 
 -- Use a function to always get a new table, even if some deep field is modified,
 -- like `config.commands.save = ...`. Returning a "constant" still seems to allow
@@ -26,23 +25,6 @@ local config = defaults()
 
 function M.setup(opts)
     local new_config = vim.tbl_deep_extend('force', {}, defaults(), opts or {})
-
-    -- Warn about deprecated RTT configuration but ignore the old settings
-    if new_config.dapui_rtt ~= nil or new_config.dapview_rtt ~= nil then
-        utils.warn_once([[
-🚨 Deprecated RTT configuration detected!
-The `dapui_rtt` and `dapview_rtt` settings are no longer used.
-Please update your configuration to use the new unified system:
-  NEW: rtt = { framework = 'auto' | 'dapui' | 'dapview' | false }
-
-Example migration:
-  OLD: dapui_rtt = true, dapview_rtt = false
-  NEW: rtt = { framework = 'auto' }  -- or 'dapui' or 'dapview'
-
-The old settings will be ignored. Using 'auto' mode (default) will
-automatically detect and use the first available framework (dapui → dapview).
-        ]])
-    end
 
     -- Do _not_ replace the table pointer with `config = ...` because this
     -- wouldn't change the tables that have already been `require`d by other
